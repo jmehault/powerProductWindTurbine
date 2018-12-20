6ff629dfdd3b47d43a6fcdc81a83e5ccb2537ab2
phypho <jemeha@gmail.com>
2018-12-20 08:03:18 +0100

    nettoie ReadFiles, GetPerformances, preprocData

--- GetPerformances.py
+++ GetPerformances.py
@@ -3,61 +3,67 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-def getMAE(yt, yp) :
-  return mean_absolute_error(yt, yp)
 
-def getAE(yt,yp) :
-  return np.abs(yt-yp)
+def getMAE(yt, yp):
+    return mean_absolute_error(yt, yp)
 
-def getMAPE(yt, yp) :
-  return np.mean(gp.getAE(yt, yp)/yt)
 
-def getResidPlot(yt, yp) :
-  mae = getMAE(yt,yp)
-  plt.subplot(2,1,1)
-  plt.plot(yt, yp,'.')
-  plt.xlabel('Observé')
-  plt.ylabel('Prédiction')
-  plt.title(f'Erreur absolue moyenne : {mae}')
-  plt.subplot(2,1,2)
-  plt.plot(yt, getAE(yt,yp), '.')
-  plt.xlabel('Observé')
-  plt.ylabel('Erreur absolue')
-  plt.show()
+def getAE(yt, yp):
+    return np.abs(yt - yp)
 
-def getAllResidPlot(ytr, yptr, yte, ypte) :
-  maetr = np.round(getMAE(ytr,yptr),2)
-  maete = np.round(getMAE(yte,ypte),2)
-  f = plt.figure(figsize=(8,10))
-  plt.subplot(2,2,1)
-  plt.plot(ytr, yptr,'.')
-  plt.plot([0,ytr.max()],[0,ytr.max()],'r.-')
-  plt.ylabel('Prédiction')
-  plt.title(f'Erreur absolue moyenne train\n{maetr}')
-  plt.subplot(2,2,3)
-  plt.plot(ytr, getAE(ytr,yptr), '.')
-  plt.xlabel('Observé train')
-  plt.ylabel('Erreur absolue')
-  plt.subplot(2,2,2)
-  plt.plot(yte, ypte,'.')
-  plt.plot([0,yte.max()],[0,yte.max()],'r.-')
-  plt.title(f'Erreur absolue moyenne test\n{maete}')
-  plt.subplot(2,2,4)
-  plt.plot(yte, getAE(yte,ypte), '.')
-  plt.xlabel('Observé test')
-  plt.show()
 
-def plotImportance(xdat, model) :
-  try :
-    # modèle simple
-    importance = pd.Series(model.feature_importances_, index=xdat.columns, name="importVal").sort_values(ascending=True)
-  except :
-    # modèle dans Pipeline
-    algo = model.named_steps['model']
-    cols = model.named_steps['selectCols'].colnames
-    importance = pd.Series(algo.feature_importances_, index=cols, name="importVal").sort_values(ascending=True)
-  posx = np.arange(len(importance))
-  importance.plot(kind='barh')
-  plt.tight_layout()
-  plt.show()
+def getMAPE(yt, yp):
+    return np.mean(getAE(yt, yp) / yt)
 
+
+def getResidPlot(yt, yp):
+    mae = getMAE(yt, yp)
+    plt.subplot(2, 1, 1)
+    plt.plot(yt, yp, '.')
+    plt.xlabel('Observé')
+    plt.ylabel('Prédiction')
+    plt.title(f'Erreur absolue moyenne : {mae}')
+    plt.subplot(2, 1, 2)
+    plt.plot(yt, getAE(yt, yp), '.')
+    plt.xlabel('Observé')
+    plt.ylabel('Erreur absolue')
+    plt.show()
+
+
+def getAllResidPlot(ytr, yptr, yte, ypte):
+    maetr = np.round(getMAE(ytr, yptr), 2)
+    maete = np.round(getMAE(yte, ypte), 2)
+    f = plt.figure(figsize=(8, 10))
+    plt.subplot(2, 2, 1)
+    plt.plot(ytr, yptr, '.')
+    plt.plot([0, ytr.max()], [0, ytr.max()], 'r.-')
+    plt.ylabel('Prédiction')
+    plt.title(f'Erreur absolue moyenne train\n{maetr}')
+    plt.subplot(2, 2, 3)
+    plt.plot(ytr, getAE(ytr, yptr), '.')
+    plt.xlabel('Observé train')
+    plt.ylabel('Erreur absolue')
+    plt.subplot(2, 2, 2)
+    plt.plot(yte, ypte, '.')
+    plt.plot([0, yte.max()], [0, yte.max()], 'r.-')
+    plt.title(f'Erreur absolue moyenne test\n{maete}')
+    plt.subplot(2, 2, 4)
+    plt.plot(yte, getAE(yte, ypte), '.')
+    plt.xlabel('Observé test')
+    plt.show()
+
+
+def plotImportance(xdat, model):
+    try:
+        # modèle simple
+        importance = pd.Series(model.feature_importances_, index=xdat.columns, name="importVal").sort_values(
+            ascending=True)
+    except:
+        # modèle dans Pipeline
+        algo = model.named_steps['model']
+        cols = model.named_steps['selectCols'].colnames
+        importance = pd.Series(algo.feature_importances_, index=cols, name="importVal").sort_values(ascending=True)
+    # posx = np.arange(len(importance))
+    importance.plot(kind='barh')
+    plt.tight_layout()
+    plt.show()
--- ReadFiles.py
+++ ReadFiles.py
@@ -2,38 +2,46 @@ import os
 import pandas as pd
 
 
-def GetInputTrainData(returnDf=True) :
-  dirData = '../Data'
-  inputData = os.path.join(dirData, 'input_training.csv')
-  try :
-    inputDf = pd.read_csv(inputData, sep=';', index_col=['ID'])
-    #inputDf = inputDf.set_index('ID')
-    return inputDf if returnDf else ' '
-  except :
-    print('no input data')
+def GetInputTrainData(returnDf=True):
+    """
+    Get input training data
+    """
+    dirData = '../Data'
+    inputData = os.path.join(dirData, 'input_training.csv')
+    try:
+        inputDf = pd.read_csv(inputData, sep=';', index_col=['ID'])
+        return inputDf if returnDf else ' '
+    except:
+        print('no input data')
 
-def GetOutputTrainData(returnDf=True) :
-  dirData = '../Data'
-  outputData = os.path.join(dirData, 'challenge_output_data_training_file_help_engie_improve_wind_power_production.csv')
-  try :
-    outputDf = pd.read_csv(outputData, sep=';', index_col=['ID'], squeeze=True)
-    #outputDf = outputDf.set_index('ID')
-    return outputDf if returnDf else ' '
-  except :
-    print('no output data')
 
-def GetAllData() :
-  inDf = GetInputTrainData(returnDf=True)
-  outDf =   GetOutputTRainData(returnDf=True)
-  df = inDf.join(outDf)
-  return df
+def GetOutputTrainData(returnDf=True):
+    """
+     Get output training data
+    """
+    dirData = '../Data'
+    filename = 'challenge_output_data_training_file_help_engie_improve_wind_power_production.csv'
+    outputData = os.path.join(dirData, filename)
+    try:
+        outputDf = pd.read_csv(outputData, sep=';', index_col=['ID'], squeeze=True)
+        # outputDf = outputDf.set_index('ID')
+        return outputDf if returnDf else ' '
+    except:
+        print('no output data')
 
-def GetInputTestData(returnDf=True) :
-  dirData = '../Data'
-  inputData = os.path.join(dirData, 'input_testing.csv')
-  try :
-    inputDf = pd.read_csv(inputData, sep=';', index_col=['ID'])
-    #inputDf = inputDf.set_index('ID')
-    return inputDf if returnDf else ' '
-  except :
-    print('no input data')
+
+def GetAllData():
+    inDf = GetInputTrainData(returnDf=True)
+    outDf = GetOutputTrainData(returnDf=True)
+    df = inDf.join(outDf)
+    return df
+
+
+def GetInputTestData(returnDf=True):
+    dirData = '../Data'
+    inputData = os.path.join(dirData, 'input_testing.csv')
+    try:
+        inputDf = pd.read_csv(inputData, sep=';', index_col=['ID'])
+        return inputDf if returnDf else ' '
+    except:
+        print('no input data')
